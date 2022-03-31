169911cbb8de955dfdc83ff4ef74f5f58f16cae0
Pertzamus <korhonen.pertti@hotmail.com>
2022-03-31 20:52:42 +0300

    file managing added, partly

---
+++ src/main/java/loppuduuni/Kouluprojekti/domain/FileModelRepository.java
@@ -0,0 +1,7 @@
+package loppuduuni.Kouluprojekti.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface FileModelRepository extends CrudRepository <FileModel, Long> {
+
+}
